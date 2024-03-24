e48e29462218c30296b3ae6c006c9bb6bef3a0f0
imtithal <halaloimtithal@gmail.com>
2024-03-25 01:43:52 +0200

    adding login and account controllers

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -33,5 +33,50 @@ const register = async (req, res) => {
     }
 }
 
+const login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
 
-module.exports = { register }
+        if (!user) {
+            return res.status(404).json({
+                message: 'User is not found'
+            })
+        }
+
+        const isValidPw = await bcrypt.compare(req.body.password, user._doc.password)
+
+        if (!isValidPw) {
+            return res.status(400).json({
+                message: 'Password is incorrect'
+            })
+        }
+
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' });
+
+        const { password, ...userData } = user._doc;
+
+        return res.status(200).json({ userData, token })
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+
+const account = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId)
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found '})
+        }
+
+        const {password, ...userData} = user._doc;
+
+        return res.status(200).json(userData)
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+module.exports = { register, login, account }
