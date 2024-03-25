72f6f8b60485c1075ac630a768114c33a8c1eb70
imtithal <halaloimtithal@gmail.com>
2024-03-25 02:48:40 +0200

    adding stories route

---
+++ api/routes/stories.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const router = Router();
+
+const { createStory, getStories, getStory, updateStory, deleteStory } = require("../controllers/story.controller");
+const protectAuth = require('../middlewares/protectAuth')
+router.post("/", protectAuth, createStory)
+router.get("/", getStories)
+router.get("/:id", getStory)
+router.put("/:id", protectAuth, updateStory)
+router.delete("/:id", protectAuth, deleteStory)
+
+module.exports = router
