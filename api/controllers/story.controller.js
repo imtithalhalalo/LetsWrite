277387146cdfbfc2d68c2cbedc843ef8b89517fa
imtithal <halaloimtithal@gmail.com>
2024-03-25 03:07:16 +0200

    adding getStory, deleteStory, updateStory functions

--- api/controllers/story.controller.js
+++ api/controllers/story.controller.js
@@ -17,13 +17,31 @@ const createStory = async (req, res) => {
 }
 
 const updateStory = async (req, res) => {
-    return res.send('This is the updateStory function')
+    try {
+        const story = await Story.findOneAndUpdate(
+            { _id: req.params.id },
+            { 
+                title: req.body.title, 
+                text: req.body.text, 
+                poster: req.body.poster,
+                author: req.userId
+            },
+            { new: true }
+        )
+
+        if (!story) {
+            return res.status(404).json({ message: 'Story is not found' })
+        }
+        return res.status(200).json(story)
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 
 const getStories = async (req, res) => {
     try {
-        const stories = await Story.find().sort({ createdAt: -1 }).populate('author').exec();
+        const stories = await Story.find().sort({ createdAt: -1 }).populate('author', '-password').exec();
 
         return res.status(200).json(stories)
     } catch (error) {
@@ -33,11 +51,38 @@ const getStories = async (req, res) => {
 
 
 const getStory = async (req, res) => {
-    return res.send('This is the getStory function')
+    try {
+        const storyId = req.params.id;
+
+        const story = await Story.findOneAndUpdate(
+            { _id: storyId },
+            { $inc: { views: 1 }},
+            { returnDocument: 'after' }
+        )
+
+        if (!story) {
+            return res.status(404).json({ message: 'Story is not found' })
+        }
+        return res.status(200).json(story)
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 const deleteStory = async (req, res) => {
-    return res.send('This is the delete function')
+    try {
+        const story = await Story.findOneAndDelete(
+            { _id: req.params.id },
+        )
+
+        if (!story) {
+            return res.status(404).json({ message: 'Story is not found' })
+        }
+        
+        return res.status(200).json(story)
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
 }
 
 module.exports = { createStory, updateStory, getStories, getStory, deleteStory }
