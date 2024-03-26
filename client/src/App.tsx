59f8e4b9422d209fd456fc92053153357d9588dc
imtithal <halaloimtithal@gmail.com>
2024-03-27 01:35:24 +0200

    adding outlet and links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,54 @@
-function App() {
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Story from "./pages/Story";
+import Write from "./pages/Write";
 
+
+const Layout = () => {
   return (
     <>
-      
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   )
 }
 
-export default App
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      }, 
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/story/:id',
+        element: <Story />
+      }, {
+        path: '/write',
+        element: <Write />
+      }
+    ]
+  }
+])
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App;
