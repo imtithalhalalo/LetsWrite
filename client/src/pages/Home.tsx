e7d7673aeaadad7f00087201a639443e605f644e
imtithal <halaloimtithal@gmail.com>
2024-04-17 20:06:15 +0300

    adding function dynamic to home story page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,62 +1,89 @@
 import { Link } from "react-router-dom"
 import HeroImg from '../assets/home.png'
 import DefaultPoster from '../assets/poster.jpg'
+import { useEffect } from "react"
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { fetchStories } from "../features/storiesSlice";
+
 
 const Home = () => {
+    const data = useAppSelector((state) => state.stories);
+    
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchStories())
+    }, [dispatch])
+
   return (
-    <div className="container-lg my-5">
-      <div className="row align-items-center align-content-center">
-          <div className="col-md-6 mt-5 mt-md-0">
-              <div className="text-center">
-                  <img src={HeroImg} alt="" className="img-fluid" height="350px" width="450px" />
-              </div>
-          </div>
-          <div className="col-md-6 mt-5 mt-md-0 order-md-first">
-              <div>
-                  <h1 className="text-info text-uppercase fs-1 fw-bold">Write Your Own Stories And Share it with Everyone!</h1>
-                  <p className="mt-4 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, tenetur
-                      sequi odit ipsam iusto cupiditate totam aut eligendi dolorem vero, repudiandae magni laudantium
-                      illum impedit nulla quis et, at omnis?</p>
-                  <Link to="/write"><button className="btn btn-info px-3 my-3 fw-bold">Write a Story</button></Link>
-              </div>
-          </div>
-      </div>
+        <div className="container-lg my-5">
+            <div className="row align-items-center align-content-center">
+                <div className="col-md-6 mt-5 mt-md-0">
+                    <div className="text-center">
+                        <img src={HeroImg} alt="" className="img-fluid" height="350px" width="450px" />
+                    </div>
+                </div>
+                <div className="col-md-6 mt-5 mt-md-0 order-md-first">
+                    <div>
+                        <h1 className="text-info text-uppercase fs-1 fw-bold">Write Your Own Stories And Share it with Everyone!</h1>
+                        <p className="mt-4 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, tenetur
+                            sequi odit ipsam iusto cupiditate totam aut eligendi dolorem vero, repudiandae magni laudantium
+                            illum impedit nulla quis et, at omnis?</p>
+                        <Link to="/write"><button className="btn btn-info px-3 my-3 fw-bold">Write a Story</button></Link>
+                    </div>
+                </div>
+            </div>
 
-      <div className="my-5">
+            <div className="my-5">
 
-          <div className="row justify-content-center">
-              <div className="col-lg-8">
-                  <div className="text-center">
-                      <h2 className="fw-bold mb-5">Latest Stories</h2>
-                  </div>
-              </div>
-          </div>
+                <div className="row justify-content-center">
+                    <div className="col-lg-8">
+                        <div className="text-center">
+                            <h2 className="fw-bold mb-5">Latest Stories</h2>
+                        </div>
+                    </div>
+                </div>
 
-          <div className="row">
-              <div className="col-md-6 col-lg-4 text-center text-decoration-none">
-                  <div className="shadow rounded">
-                      <img src={DefaultPoster} alt="" height="300px" width="200px" />
-                  </div>
-                  <h2 className="lead fw-bold my-4">Story 1</h2>
-                  <Link to="/story/:id"><button className="btn btn-info text-white lead fw-bold mb-5">Read Story</button></Link>
-              </div>
-              <div className="col-md-6 col-lg-4 text-center text-decoration-none">
-                  <div className="shadow rounded">
-                      <img src={DefaultPoster} alt="" height="300px" width="200px" />
-                  </div>
-                  <h2 className="lead fw-bold my-4">Story 2</h2>
-                  <Link to="/story/:id"><button className="btn btn-info text-white lead fw-bold mb-5">Read Story</button></Link>
-              </div>
-              <div className="col-md-6 col-lg-4 text-center text-decoration-none">
-                  <div className="shadow rounded">
-                      <img src={DefaultPoster} alt="" height="300px" width="200px" />
-                  </div>
-                  <h2 className="lead fw-bold my-4">Story 3</h2>
-                  <Link to="/story/:id"><button className="btn btn-info text-white lead fw-bold mb-5">Read Story</button></Link>
-              </div>
-          </div>
-      </div>
-    </div>
+                {
+                    data.status === 'loading' && (
+                        <div className="text-center mt-5">
+                            <h3 className="text-secondary fw-bold fs-4">
+                                Loading...
+                            </h3>
+
+                        </div>
+                    )
+                }
+
+                {
+                    data.status === 'error' && (
+                        <div className="text-center mt-5">
+                            <h3 className="text-secondary fw-bold fs-4">
+                                Oops something went wrong.
+                            </h3>
+
+                        </div>
+                    )
+                }
+                
+                <div className="row">
+                    {
+                        data.status === 'success' && data.stories.map((story: any) => {
+                            console.log(story._id)
+                            return (
+                            <div className="col-md-6 col-lg-4 text-center text-decoration-none">
+                                <div className="shadow rounded">
+                                    <img src={story.poster ? `http://localhost:5000/${story.poster}` : DefaultPoster} alt={story.title} height="300px" width="200px" />
+                                </div>
+                                <h2 className="lead fw-bold my-4">{story.title}</h2>
+                                <Link to={`/story/${story._id}`}><button className="btn btn-info text-white lead fw-bold mb-5">Read Story</button></Link>
+                            </div>
+)})
+                    }
+                   
+                </div>
+            </div>
+        </div>
   )
 }
 
